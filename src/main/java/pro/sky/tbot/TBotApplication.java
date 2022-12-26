45f3d447b54534abd12689dc9980122310795201
user <grebneva.nastena@yandex.ru>
2022-12-26 12:00:14 +0300

    Initial commit

---
+++ src/main/java/pro/sky/tbot/TBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.tbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/tbot/TBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.tbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
